6a9826d0f06a09f43f5ca97bd9438da071b5a725
Sunny Moore <moore.sunny@gmail.com>
2022-04-13 19:10:10 -0700

    Change variable values

--- variables.rb
+++ variables.rb
@@ -9,3 +9,23 @@ cats = 1
 weight = 125.5
 temp = 50.5
 oven_temp = 400.0
+
+is_done = true
+is_hungry = true
+is_cleaned = false
+
+dinner = "spaghetti"
+side_dish = "salad"
+drink = "soda"
+
+guests = 5
+dogs = 0
+cats = 6
+
+weight = 45.2
+temp = 78.9
+oven_temp = 0.0
+
+is_done = false
+is_hungry = false
+is is_cleaned = true
